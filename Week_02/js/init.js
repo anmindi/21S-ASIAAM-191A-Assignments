46dc5e2ae092fbd0e7705a5ae98bad3ed95f6e3c
anmindi <anmin@g.ucla.edu>
2021-04-16 16:53:13 -0700

    finished assignment

--- Week_02/js/init.js
+++ Week_02/js/init.js
@@ -1,5 +1,5 @@
 // JavaScript const variable declaration
-const map = L.map('map').setView([34.0709, -118.444], 15);
+const map = L.map('map').setView([34.0709, -118.444], 5);
 
 // Leaflet tile layer, i.e. the base map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,18 +7,36 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 //JavaScript let variable declaration to create a marker
-let marker = L.marker([34.0709, -118.444]).addTo(map)
-		.bindPopup('Math Sciences 4328 aka the Technology Sandbox<br> is the lab where I work in ')
+let marker = L.marker([34.0945, -118.0905]).addTo(map)
+		.bindPopup('✿ San Gabriel Nursery & Florist (a local, Asian-owned garden supply center founded in 1923)')
 		.openPopup();
 
-		fetch("js/map.geojson")
-		.then(response => {
-			return response.json()
+let marker2 = L.marker([33.0945, -118.0905]).addTo(map)
+		.bindPopup('✿ San Gabriel Nursery & Florist (a local, Asian-owned garden supply center founded in 1923)')
+		.openPopup();
+
+
+fetch("js/map.geojson")
+	.then(response => {
+		return response.json();
 		})
-		.then(data =>{
-			// Basic Leaflet method to add GeoJSON data
-			L.geoJSON(data, myLayerOptions)
-			.bindPopup(function (layer) {
-				return layer.feature.properties.place;
-			}).addTo(map);
-		})
+    .then(data =>{
+        // Basic Leaflet method to add GeoJSON data
+                        // the leaflet method for adding a geojson
+            L.geoJSON(data, {
+                style: function (feature) {
+                    return {color: 'red'};
+                }
+            }).bindPopup(function (layer) {
+                return layer.feature.properties.name;
+            }).addTo(map);
+        });
+
+		function customMarker (feature, latlng) {
+			return L.circleMarker(latlng, { color: feature.properties.color })
+		  }
+		  
+		  // create an options object
+		  let myLayerOptions = {
+			pointToLayer: customMarker
+		  }
