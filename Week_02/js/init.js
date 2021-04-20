076b8e98c626176912234be7018b36ad83fd651c
anmindi <anmin@g.ucla.edu>
2021-04-19 20:48:44 -0700

    push

--- Week_02/js/init.js
+++ Week_02/js/init.js
@@ -1,5 +1,5 @@
 // JavaScript const variable declaration
-const map = L.map('map').setView([34.0709, -118.444], 5);
+const map = L.map('map').setView([36.778259, -119.417931], 5);
 
 // Leaflet tile layer, i.e. the base map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,12 +7,20 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 //JavaScript let variable declaration to create a marker
-let marker = L.marker([34.0945, -118.0905]).addTo(map)
-		.bindPopup('✿ San Gabriel Nursery & Florist (a local, Asian-owned garden supply center founded in 1923)')
+let marker = L.marker([34.0365, -118.2465]).addTo(map)
+		.bindPopup('✿ API Forward Movement')
 		.openPopup();
 
-let marker2 = L.marker([33.0945, -118.0905]).addTo(map)
-		.bindPopup('✿ San Gabriel Nursery & Florist (a local, Asian-owned garden supply center founded in 1923)')
+let marker2 = L.marker([34.0014, -118.3311]).addTo(map)
+		.bindPopup('✿ Asian American Drug Abuse Program')
+		.openPopup();
+
+let marker3 = L.marker([36.9977, -122.0555]).addTo(map)
+		.bindPopup('✿ Psychology Lab with Dr. Azmitia')
+		.openPopup();
+
+let marker4 = L.marker([37.0016, -122.0587]).addTo(map)
+		.bindPopup('✿ Asian American/Pacific Islander Resource Center')
 		.openPopup();
 
 
--- Week_02/readme.md
+++ Week_02/readme.md
@@ -1,2 +1,4 @@
 # Week 2 Assignment
 https://anmindi.github.io/21S-ASIAAM-191A-Assignments/
+
+https://anmindi.github.io/21S-ASIAAM-191A-Assignments/Week_02/index.html
