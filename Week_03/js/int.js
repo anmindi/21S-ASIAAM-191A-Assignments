ddbada9ba44768db75e856987b1c50d244c8e162
anmindi <anmin@g.ucla.edu>
2021-07-26 17:54:45 -0700

    commit

--- Week_03/js/int.js
+++ Week_03/js/int.js
@@ -1,4 +1,12 @@
-const map = L.map('map').setView([34.0709, -118.444], 5);
+let a = [34.0709, -118.444];
+let b = 5;
+
+let mapConfig = {"center": [34.0709, -118.444], "zoomLevel":5}
+
+
+const map = L.map('map').setView(mapConfig.center, mapConfig.zoomLevel);
+
+
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -13,10 +21,3 @@ let home = L.marker([37.7409, -122.484]).addTo(map)
 
 let random = L.marker([39.7409, -122.484]).addTo(map)
 		.bindPopup('Third Point')
-
-        function addMarker(lat,lng,message){
-            console.log(message)
-            L.marker([lat,lng]).addTo(map).bindPopup(message)
-            return message
-        }
-        
