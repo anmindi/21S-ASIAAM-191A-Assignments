06437a87de5aab0e94cda38c864a54c747c2a3c0
anmindi <anmin@g.ucla.edu>
2021-07-28 22:54:38 -0700

    commit

--- Week_04/js/init.js
+++ Week_04/js/init.js
@@ -4,10 +4,11 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-let url = "GIVE_ME_A_URL"
+let url = "https://spreadsheets.google.com/feeds/list/1dByZZ7fDh0efw8-7_Fw1UXqHVMbFGLnbPL-cWLrUfMY/ohp2aiq/public/values?alt=json"
 fetch(url)
 	.then(response => {
 		return response.json();
 		})
     .then(data =>{
-        console.log(data)})
+        console.log(data)
+    })
